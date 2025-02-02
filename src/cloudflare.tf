4c71b4e8275bc67f96abf8f409dbca71a8939045
Philip Welz <philip.welz@whiteduck.de>
2025-02-02 23:31:12 +0100

    Feat/tf (#2)
    chore: update resource names, add tags to resources

--- src/cloudflare.tf
+++ src/cloudflare.tf
@@ -1,9 +1,9 @@
-data "cloudflare_zone" "philipwelz" {
+data "cloudflare_zone" "default" {
   name = var.custom_domain
 }
 
-resource "cloudflare_record" "chat" {
-  zone_id = data.cloudflare_zone.philipwelz.id
+resource "cloudflare_record" "librechat" {
+  zone_id = data.cloudflare_zone.default.id
   name    = var.host
   content = azurerm_container_app.librechat.ingress[0].fqdn
   type    = "CNAME"
@@ -11,7 +11,7 @@ resource "cloudflare_record" "chat" {
 }
 
 resource "cloudflare_record" "verification" {
-  zone_id = data.cloudflare_zone.philipwelz.id
+  zone_id = data.cloudflare_zone.default.id
   name    = "asuid.chat"
   content = azurerm_container_app.librechat.custom_domain_verification_id
   type    = "TXT"
